72c091c003f8110093ed60b5c1062495ad023e1b
xutengx <68822684@qq.com>
2021-04-28 23:01:06 +0800

    leetcode-23

---
+++ leetcode/editor/cn/23-merge-k-sorted-lists.cpp
@@ -0,0 +1,111 @@
+//给你一个链表数组，每个链表都已经按升序排列。 
+//
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+//输出：[1,1,2,3,4,4,5,6]
+//解释：链表数组如下：
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//将它们合并到一个有序链表中得到。
+//1->1->2->3->4->4->5->6
+// 
+//
+// 示例 2： 
+//
+// 输入：lists = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 输入：lists = [[]]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// k == lists.length 
+// 0 <= k <= 10^4 
+// 0 <= lists[i].length <= 500 
+// -10^4 <= lists[i][j] <= 10^4 
+// lists[i] 按 升序 排列 
+// lists[i].length 的总和不超过 10^4 
+// 
+// Related Topics 堆 链表 分治算法 
+// 👍 1279 👎 0
+
+
+#include "functional"
+#include "vector"
+#include "assert.h"
+#include "iostream"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if(lists.size() == 0){
+            return nullptr;
+        }
+
+        ListNode * temp = lists[0];
+        for (int i = 1; i < lists.size(); i++) {
+            temp = merge(temp, lists[i]);
+        }
+        return temp;
+    }
+
+    static ListNode * merge(ListNode * one, ListNode * two){
+        ListNode * head = new ListNode;
+        ListNode * temp = head;
+        while (one != nullptr && two != nullptr){
+            if(one->val <= two->val){
+                temp->next = one;
+                one = one->next;
+            }else{
+                temp->next = two;
+                two = two->next;
+            }
+            temp = temp->next;
+        }
+        if (one != nullptr){
+            temp->next = one;
+        }
+        if (two != nullptr){
+            temp->next = two;
+        }
+        return head->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main(){
+    Solution solution;
+    //
+    
+    return 0;
+}

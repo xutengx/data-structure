bb3bc53c28aa19325c23789cc41091a8bb0cd2dc
xut05 <xut05@mingyuanyun.com>
2021-05-01 16:55:42 +0800

    leetcode-19

---
+++ leetcode/editor/cn/19-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,100 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+// Related Topics 链表 双指针 
+// 👍 1344 👎 0
+
+
+#include "iostream"
+#include "cassert"
+#include "vector"
+#include "string"
+#include "functional"
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        if (n <= 0) {
+            return head;
+        }
+        auto *flagHead = new ListNode;
+        flagHead->next = head;
+
+        ListNode *pNode = flagHead;
+        ListNode *bNode = flagHead;     // 末节点
+        while (n--) {
+            bNode = bNode->next;
+        }
+        while (bNode->next != nullptr) {
+            pNode = pNode->next;
+            bNode = bNode->next;
+        }
+        pNode->next = pNode->next->next;
+        return flagHead->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution solution;
+    // 
+    return 0;
+}

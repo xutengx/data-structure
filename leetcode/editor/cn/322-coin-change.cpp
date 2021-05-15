b566faf63292cfa924e458dd3c16f4f6a920343a
xutengx <68822684@qq.com>
2021-05-15 15:37:29 +0800

    leetcode-322

--- leetcode/editor/cn/322-coin-change.cpp
+++ leetcode/editor/cn/322-coin-change.cpp
@@ -57,40 +57,97 @@
 #include "vector"
 #include "string"
 #include "functional"
+#include "unordered_map"
+#include "algorithm"
 
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+
+    /**
+     * 升序
+     * @param a
+     * @param b
+     * @return
+     */
+    static bool compare(int a, int b) {
+        return a > b;
+    }
+
     /**
      * 思路类似于青蛙跳楼梯，假如coins = [1,2,5],amout = 11 那么就是11可以从dp[10],dp[9],dp[6]3个地方到达， 所以状态dp[n] = min(dp[10],dp[9],dp[6]) + 1 就是表示n最少的方式
      * dp(n) = min{dp(n - coins[0]),dp(n - coins[1]), ...}
      * 时间复杂度 = amout * len(coins) ,以为二维表就这么大 空间复杂度 = amout
      */
+    /**
+
+     */
     int coinChange(vector<int> &coins, int amount) {
-        unsigned int size = coins.size();
-
-        int *cache = new int[size + 1];
-        for (int i = 0; i < size; ++i) {
-            cache[amount[i]] = 0;
-        }
-        function<int(int)> coinChangeReal;
-
-        coinChangeReal = [&coinChangeReal, &size, &coins](int amounts) {
-            if (amounts){
-
-            }
-            int min = INT_MAX;
-            for (int i = 0; i < size; ++i) {
-                int minI = coinChangeReal(amounts - coins[i]);
-                if (min > minI) {
-                    min = minI;
+        int Max = amount + 1;
+        // 1、状态数组DP table
+        // 建长为amount+1的数组，且每个元素也初始化为amount+1（因为凑成amount金额的硬币数最多只可能等于amount（全用 1 元面值的硬币）），
+        // 在遍历时如果金额凑不出则不更新，于是若最后结果仍然是amount + 1，则表示金额凑不出，返回-1
+        vector<int> dp(amount + 1, Max);
+        // 2、状态初始化
+        dp[0] = 0;
+        // 3、状态转移
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j <= coins.size()-1; ++j) {
+                if (i >=coins[j]) {   //为了避免出现dp[2-5]的情况
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                    //和剪绳子一样，加了上一轮j的dp[i]
                 }
             }
+        }
+        //4、返回最终状态
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    /**
+     * 解答成功:
+     * 执行耗时:672 ms,击败了5.04% 的C++用户
+     * 内存消耗:60.6 MB,击败了5.01% 的C++用户
+     */
+    int coinChange(vector<int> &coins, int amount) {
+        // 提前响应
+        if(amount == 0){
+            return 0;
+        }
+        unordered_map<int, int> cache;
+        // 倒序排列
+        sort(coins.begin(), coins.end(), compare);
+        unsigned int size = coins.size();
+        // 边界终止
+        if(size == 0){
+            return -1;
+        }
+
+        function<int(int)> coinChangeReal;
+
+        coinChangeReal = [&coinChangeReal, &size, &coins, &cache](int amounts) {
+            if(cache.count(amounts)){
+                return cache[amounts];
+            }
+            int min = -1;
+            for (int i = 0; i < size; ++i) {
+                if (amounts - coins[i] > 0) {
+                    int minI = coinChangeReal(amounts - coins[i]);
+                    if (minI == -1) {
+                        continue;
+                    } else if (min == -1 || min > minI) {
+                        min = minI + 1;
+                    }
+                } else if (amounts - coins[i] == 0) {
+                    min = 1;
+                    break;
+                }
+            }
+            cache[amounts] = min;
             return min;
         };
-
+        return coinChangeReal(amount);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
@@ -98,6 +155,24 @@ public:
 
 int main() {
     Solution solution;
-    // 
+    //
+
+    vector<int> t0 = {1,4,5};
+    assert(solution.coinChange(t0, 8)== 2);
+
+    vector<int> t1 = {1,2,5};
+    int i = solution.coinChange(t1, 100);
+    assert(solution.coinChange(t1, 100)== 20);
+
+    vector<int> t2 = {186,419,83,408};
+    assert(solution.coinChange(t2, 6249)== 20);
+
+    vector<int> t3 = {3};
+    assert(solution.coinChange(t3, 3)== 1);
+
+    vector<int> t4 = {1,2,3,4,5};
+    assert(solution.coinChange(t4, 5)== 1);
+    vector<int> t5 = {1,2,3,4,5};
+    assert(solution.coinChange(t5, 9)== 2);
     return 0;
 }
